0b55de7e2c65cf6429bbf65196f5c944eac8a99b
Weerapat Thawatchoketawee <weerapat@central.tech>
2018-10-30 12:17:31 +0700

    UPdate Button Sass

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const Home = () => (
   <div>
-    Home
+    <h4>Home</h4>
+    <div style={{ display: 'flex' }}>
+      <button className="btn btn--default">Default</button>
+      <button className="btn btn--primary">Primary</button>
+      <button className="btn btn--info">Info</button>
+      <button className="btn btn--warning">Warning</button>
+      <button className="btn btn--success">Success</button>
+      <button className="btn btn--danger">Danger</button>
+      <button className="btn btn--disabled" disabled>Disable</button>
+    </div>
+    <div style={{ display: 'flex' }}>
+      <button className="btn btn--large btn--default">Default</button>
+      <button className="btn btn--large btn--primary">Primary</button>
+      <button className="btn btn--large btn--info">Info</button>
+      <button className="btn btn--large btn--warning">Warning</button>
+      <button className="btn btn--large btn--success">Success</button>
+      <button className="btn btn--large btn--danger">Danger</button>
+      <button className="btn btn--large btn--disabled" disabled>Disable</button>
+    </div>
   </div>
 );
 
